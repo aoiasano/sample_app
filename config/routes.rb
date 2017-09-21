22b9346c100efb8d990b4aa9c8f50a0a5aadefae
Aoi Asano <aasano0425@gmail.com>
2017-09-21 00:14:37 +0000

    Display a user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 SampleApp::Application.routes.draw do
+  get "user/show"
   resources :accounts
-
+  
   devise_for :users
+  resources :users, only: [:show]
   root  'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
